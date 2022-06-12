d1a96be3b9dfee69c823666db6acfc2df936c73f
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2022-06-12 15:44:58 -0400

    added tilt image to landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,8 +12,8 @@ export default function Index() {
             <div className="absolute inset-0">
               <img
                 className="h-full w-full object-cover"
-                src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
-                alt="Sonic Youth On Stage"
+                src="https://crossfittiltwaltham.com/wp-content/uploads/2021/05/Waltham-Main1.jpg"
+                alt="CrossFit Tilt-Waltham Members in the Gym"
               />
               <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
             </div>
