eb1cd60f59ab1f4e1202a60a1a71065b05ad9baa
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2022-06-18 10:16:05 -0400

    scaffolding for calendar component with fake data

--- app/routes/admin/challenges.tsx
+++ app/routes/admin/challenges.tsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from "@remix-run/react";
 
 export default function AdminChallengesLayoutPage() {
     return (
-        <div>
+        <div className="max-w-5xl">
             TODO: This is the admin layout for managing challenges. This content is the same for all the challenge related admin activities
             <Outlet />
         </div>
--- app/routes/admin/challenges/index.tsx
+++ app/routes/admin/challenges/index.tsx
@@ -1,6 +1,6 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { json, LoaderFunction } from "@remix-run/node";
-import { getChallengesByAdminId } from "~/models/challenge.server";
+import { ChallengeWithActivities, getChallengesByAdminId } from "~/models/challenge.server";
 
 export type LoaderData = {
     challenges: Awaited<ReturnType<typeof getChallengesByAdminId>>;
@@ -20,9 +20,33 @@ export default function AdminChallengesIndexPage() {
     return (
         <div>
             TODO: add dashboard for the various admin duties
+            <div className="grid grid-cols-[repeat(auto-fit,385px)] gap-4  ">
+                {loaderData.challenges.map((challenge) => (
+                    <ChallengeItem key={challenge.id} challenge={challenge} />
+                ))}
+            </div>
             <pre><code>
                 {JSON.stringify(loaderData, null, 2)}
             </code></pre>
         </div>
     );
+}
+
+function ChallengeItem({ challenge }: { challenge: ChallengeWithActivities }) {
+    const startDate = challenge.startDate.toString();
+    const endDate = challenge.endDate.toString()
+
+    return (
+        <div key={challenge.id} className="p-6  bg-white rounded-xl shadow-lg flex flex-col items-start ">
+            <h3 className="text-2xl mb-4 font-bold">{challenge.title}</h3>
+            <p className="mb-2">
+                <span className="block font-bold">When:</span>
+                <time dateTime={startDate}>{challenge.startDate}</time> through <time dateTime={endDate}>{challenge.endDate}</time>
+            </p>
+            <p className="mb-6">
+                <span className="block font-bold">What:</span> {challenge.description}
+            </p>
+            <Link to={`${challenge.id}`}>View</Link>
+        </div>
+    )
 }
