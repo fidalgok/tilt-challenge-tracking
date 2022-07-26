faaa048bfe4095ef63c672ce375326a5392419fb
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2022-07-26 11:02:24 -0400

    added description disclosure to mobile styling

--- app/routes/challenges/$challengeId/index.tsx
+++ app/routes/challenges/$challengeId/index.tsx
@@ -64,7 +64,27 @@ export default function ChallengeEntries() {
 
     return (
         <>
-            <p className="py-6 whitespace-pre-line" dangerouslySetInnerHTML={{ __html: challenge.description }}></p>
+            <div className="col-span-3 md:hidden">
+
+
+                <Disclosure>
+                    {({ open }) => (
+                        <>
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-slate-100 px-4 py-2 text-left text-sm font-medium text-slate-900 hover:bg-slate-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                                <span>Challenge Description</span>
+                                <ChevronUpIcon
+                                    className={`${open ? 'rotate-180 transform' : ''
+                                        } h-5 w-5 text-slate-500`}
+                                />
+                            </Disclosure.Button>
+                            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
+                                <p className="py-6 whitespace-pre-line" dangerouslySetInnerHTML={{ __html: challenge.description }}></p>
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
+            </div>
+            <p className="hidden md:block py-6 whitespace-pre-line" dangerouslySetInnerHTML={{ __html: challenge.description }}></p>
             {isBefore(new Date(), new Date(parseDateStringFromServer(challenge.startDate.toString()))) ? (
                 <div className="flex justify-center items-center flex-col">
                     <div className="max-w-md mt-12 mb-8">
