355d83c7f40d6d84fadec93b53162d2296166de9
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2022-09-05 11:34:22 -0400

    basic layout complete for archived challenges

--- app/routes/challenges/complete.$challengeId.tsx
+++ app/routes/challenges/complete.$challengeId.tsx
@@ -3,7 +3,7 @@ import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { getChallenge, getChallengeEntries, getChallengeLeaderboard, getTotalAmount } from "~/models/challenge.server";
 import { requireUserId } from "~/session.server";
-import { useLoaderData } from "@remix-run/react";
+import { useCatch, useLoaderData, useParams } from "@remix-run/react";
 
 
 type LeaderBoardReduceReturnType = {
@@ -49,35 +49,37 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 
     return json({ challenge, entries, totalVolume: aggregation._sum.amount, leaderboard, userId: userId })
 }
+
 export default function ChallengeCompleteRoute() {
     const data = useLoaderData<typeof loader>();
     let idx = data.leaderboard.findIndex(p => p.userId === data.userId);
     let rank = idx === -1 ? 'something bad happened' : idx + 1;
     return (
         <div>
-            This challenge is complete! Here are the results.
-            <div className="flex gap-6">
-                <div>
-                    <h2>Your Stats</h2>
-                    <div>
+            <p className="text-2xl font-semibold mb-8">{data.challenge.title}{" "}Results
+            </p>
+            <div className="flex gap-8">
+                <div className="rounded-lg shadow-sm ring-1 ring-black ring-opacity-5 col-span-3 md:col-span-2 xl:col-auto py-3 px-4">
 
-                        <div>
-                            <h3>Your Rank</h3>
-                            <p>{rank}</p>
+                    <div>
+                        <div className="mb-6">
+                            <h3 className="text-2xl font-semibold mb-4">Total {data.challenge.activity[0].unit}</h3>
+                            <p className="text-center text-lg">{data.totalVolume}</p>
                         </div>
                         <div>
-                            <h3>Total {data.challenge.activity[0].unit}</h3>
-                            <p>{data.totalVolume}</p>
+                            <h3 className="text-2xl font-semibold mb-4">Your Rank</h3>
+                            <p className="text-center text-lg">{rank}</p>
                         </div>
+
 
 
 
                     </div>
                 </div>
-                <div>
-                    <h2>The Leaderboard</h2>
+                <div className="rounded-lg shadow-sm ring-1 ring-black ring-opacity-5 col-span-3 md:col-span-2 xl:col-auto py-3 px-4">
+                    <h3 className="text-2xl font-semibold mb-4">The Leaderboard</h3>
                     {data.leaderboard.map((entry, index) => (
-                        <div key={`${entry.userId}`} className="flex border-b border-slate-100 py-3 px-4 last:border-b-0">
+                        <div key={`${entry.userId}`} className="flex border-b border-slate-100 pb-3 px-4 last:border-b-0">
                             <div className="mr-4 flex items-center text-lg font-bold">{index + 1}</div>
                             <div className="grow flex flex-col">
                                 {entry.name}
@@ -91,10 +93,35 @@ export default function ChallengeCompleteRoute() {
 
 
             </div>
-            <div>
-                <h3>Your Entries</h3>
-                <div>TODO: show entries</div>
-            </div>
+
         </div>
     )
+}
+
+export function CatchBoundary() {
+    const caught = useCatch();
+    const params = useParams();
+
+    if (caught.status === 404) {
+        return (
+            <div className="p-12 text-red-500">
+                No challenge found with the ID of "{params.challengeId}"
+            </div>
+        );
+    }
+
+    throw new Error(`Unexpected caught response with status: ${caught.status}`);
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+    console.error(error);
+
+    return (
+        <div className="absolute inset-0 flex justify-center bg-red-100 pt-4">
+            <div className="text-red-brand text-center">
+                <div className="text-[14px] font-bold">Oh snap!</div>
+                <div className="px-2 text-[12px]">There was a problem. Sorry.</div>
+            </div>
+        </div>
+    );
 }
